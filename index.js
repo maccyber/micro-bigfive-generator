ab2b3854327ce31dbc201cd38e55328e3470179e
maccyber <jonas.enge@gmail.com>
2017-02-16 00:38:53 +0100

    genesis commit

--- index.js
+++ index.js
@@ -39,5 +39,7 @@ module.exports = async (req, res) => {
   }
   let status = result.error ? 500 : 200
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
   send(res, status, result)
 }
